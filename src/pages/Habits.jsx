f9a346bf2380795d2bb550b2244b5a1882c7789d
Jef Gannaban <jefjef1412@gmail.com>
2025-06-28 23:40:39 +0800

    created modal -Newnote and added esc navigation

--- src/pages/Habits.jsx
+++ src/pages/Habits.jsx
@@ -4,15 +4,17 @@ import Contentcard from '../components/Contentcard'
 import Sidebar from '../components/Sidebar.jsx';
 import Maincontent from '../components/Maincontent.jsx';
 import Notedetail from '../components/Notedetail.jsx'; 
+import Newnote from '../components/Newnote.jsx';
 
 function Habits() {
     const [selectedNote, setSelectedNote] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     return(
         <div className="h-screen w-screen bg-[#FFEE9F] flex flex-row justify-center px-10 pt-16 pb-16 gap-7"
              style={{ backgroundImage: `url(${scattered_icons})`}}>
                 {/* Sidebar */}
-                 <Sidebar/>
+                <Sidebar onCreateClick={() => setIsModalOpen(true)} />
                  
                 {/* Main Content */}
                  <Maincontent title="Habits" isSub={true} isOpen={selectedNote} >
@@ -23,6 +25,8 @@ function Habits() {
                 </Maincontent>
 
                 {selectedNote && (<Notedetail note={selectedNote} onClose={() => setSelectedNote(null)} />)}
+                {isModalOpen &&(<Newnote isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} onSave={(note) => console.log('Saving note:', note)}  />)}
+                
         </div>
     )
 }
